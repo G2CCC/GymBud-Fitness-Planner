b8e86fcb815dbe9d7f5bb6bc1c29aef3fb94d31b
G2CCC <garycheung0307@gmail.com>
2026-09-16 12:50:05 +1200

    fix .env path

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,4 +1,9 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+import { fileURLToPath } from "node:url";
+
+dotenv.config({
+  path: fileURLToPath(new URL("../../../.env", import.meta.url)),
+});
 
 export type AuthProvider = "supabase" | "fake";
 
